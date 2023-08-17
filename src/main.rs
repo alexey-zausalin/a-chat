2753c641e54ef457c12403b8b28eb60a8e06853a
Oleksiy Zausalin <oleksiy.zausalin@yalantis.net>
2023-08-17 12:47:45 +0300

    Writing an Accept Loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::{
+    net::{TcpListener, ToSocketAddrs},
+    prelude::*,
+    task,
+};
+
+fn main() -> Result<()> {
+    let fut = accept_loop("127.0.0.1:8000");
+    task::block_on(fut)
+}
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    let mut incoming = listener.incoming();
+    while let Some(_stream) = incoming.next().await {
+        todo!();
+    }
+
+    Ok(())
 }
